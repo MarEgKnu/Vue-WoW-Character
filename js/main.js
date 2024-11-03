c5cfc23e1302f863ed8b1d453a618ee3dde0cf3d
Marius Egede Knudsen <mak004@edu.zealand.dk>
2024-11-03 22:12:13 +0100

    added more frontend

--- js/main.js
+++ js/main.js
@@ -4,24 +4,63 @@ const app = Vue.createApp({
 
         return {
             characters: [
-                {
-                    name: "Unknown",
-                    level: 0,
-                    stamina: 0,
-                    strength: 0,
-                    intellect: 0,
-                    spirit: 0,
-                    agility: 0,
-                }
+
+            ],
+            characterInput: {
+                name: "Unknown",
+                class: "Unknown",
+                level: 0,
+                stamina: 0,
+                strength: 0,
+                intellect: 0,
+                spirit: 0,
+                agility: 0,
+            },
+            maxLevel: 80,
+            minLevel: 1,
+            classes: [
+
             ]
+
         }
 
     },
 
     methods: {
-        myMethod() {
-
+        characterSummary(character) {
+            return character.name + ": " + "Level " + character.level + " " + character.class
         },
+        addCharacter() {
+            if (this.validateCharacterInput()) {
+                this.characters.push( {
+                    name: this.characterInput.name,
+                    class: this.characterInput.class,
+                    level: this.characterInput.level,
+                    stamina: this.characterInput.stamina,
+                    strength: this.characterInput.strength,
+                    intellect: this.characterInput.intellect,
+                    spirit: this.characterInput.spirit,
+                    agility: this.characterInput.agility,
+                })
+            }
+            
+        },
+        validateCharacterInput() {
+            if (!Number.isInteger(this.characterInput.level) || this.characterInput.level > this.maxLevel || this.characterInput.level < this.minLevel) {
+                window.alert("Invalid level value")
+                return false
+            }
+            else if (!Number.isInteger(this.characterInput.stamina) || this.characterInput.stamina < 1) {
+                window.alert("Invalid stamina value")
+                return false
+            }
+            else if (!Number.isInteger(this.characterInput.strength) || this.characterInput.strength < 1) {
+                window.alert("Invalid strength value")
+                return false
+            }
+            return true
+        }
+        
     
     },
     computed: {
